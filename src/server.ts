9e684e241edfed54ba6b0ac57f3760995c7f6225
matheus willock <matheuswillock@outlook.com>
2023-03-15 22:51:10 -0300

    ajustes

--- src/server.ts
+++ src/server.ts
@@ -7,8 +7,7 @@ const app = fastify()
 const prisma = new PrismaClient()
 
 app.get('/users', async () => {
-  const users = await prisma.user.findMany()
-
+  const users = await prisma.user.findMany();
   return { users }
 })
 
